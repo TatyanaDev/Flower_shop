33ff489b051a137da6060e947c3f616dda34f5ae
Tatyana404 <kot.kate2020@gmail.com>
2025-01-15 13:42:47 +0100

    changed Russia to Ukraine and added images of bouquets locally

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,92 +33,92 @@ $(document).ready(() => {
       [
         {
           src:
-            'https://www.roza4u.ru/image/cache/catalog/25-krasnyh-roz-s-jevkaliptom/25-krasnyh-roz-s-jevkaliptom-1400x1400.jpg',
+            'assets/images/bouquets/bouquet-of-25-red-roses-with-eucalyptus.jpg',
           opts: {
             caption: 'Букет 25 красных роз с эвкалиптом',
             thumb:
-              'https://www.roza4u.ru/image/cache/catalog/25-krasnyh-roz-s-jevkaliptom/25-krasnyh-roz-s-jevkaliptom-1400x1400.jpg'
+              'assets/images/bouquets/bouquet-of-25-red-roses-with-eucalyptus.jpg'
           }
         },
         {
           src:
-            'https://kartinkin.net/uploads/posts/2022-02/1645775353_11-kartinkin-net-p-romashki-buket-kartinki-12.jpg',
+            'assets/images/bouquets/big-bouquet-of-daisies.jpg',
           opts: {
             caption: 'Большой букет ромашек',
             thumb:
-              'https://kartinkin.net/uploads/posts/2022-02/1645775353_11-kartinkin-net-p-romashki-buket-kartinki-12.jpg'
+              'assets/images/bouquets/big-bouquet-of-daisies.jpg'
           }
         },
         {
           src:
-            'https://zarum.ru/uploads/posts/2020-07/1596194854_c256ae5f-29b8-458a-b35e-5fc05aff4320.jpeg',
+            'assets/images/bouquets/bouquet-of-purple-hydrangea.jpg',
           opts: {
             caption: 'Букет из фиолетовой гортензии',
             thumb:
-              'https://zarum.ru/uploads/posts/2020-07/1596194854_c256ae5f-29b8-458a-b35e-5fc05aff4320.jpeg'
+              'assets/images/bouquets/bouquet-of-purple-hydrangea.jpg'
           }
         },
         {
           src:
-            'https://million-buketov.ru/thumb/2/AceEMTzmKQ9O0OXz_HoRaw/r/d/buket-iz-vasilkov-s-upakovkoy.jpg',
+            'assets/images/bouquets/big-bouquet-of-cornflowers.jpg',
           opts: {
             caption: 'Большой букет васильков',
             thumb:
-              'https://million-buketov.ru/thumb/2/AceEMTzmKQ9O0OXz_HoRaw/r/d/buket-iz-vasilkov-s-upakovkoy.jpg'
+              'assets/images/bouquets/big-bouquet-of-cornflowers.jpg'
           }
         },
         {
           src:
-            'https://static.insalescdn.com/images/products/1/3456/242945408/514-1.jpg',
+            'assets/images/bouquets/bouquet-of-raspberry-hydrangea.jpg',
           opts: {
             caption: 'Букет из малиновой гортензии',
             thumb:
-              'https://static.insalescdn.com/images/products/1/3456/242945408/514-1.jpg'
+              'assets/images/bouquets/bouquet-of-raspberry-hydrangea.jpg'
           }
         },
         {
           src:
-            'https://lh4.googleusercontent.com/-LK7MMd-SvbyI-wezk3n_gADj6U0kgAIJQ1neZv1KtxJk5fsmH24x6VrXg5ZC6GYKW-B0XLN5n33S7ZHDq7D-2-DUKvV3BtlODc5a1kQODlDYCd976-_GZo577LvcRrzsgPNAlo6',
+            'assets/images/bouquets/bouquet-of-orange-lilies.jpg',
           opts: {
             caption: 'Букет оранжевых лилий',
             thumb:
-              'https://lh4.googleusercontent.com/-LK7MMd-SvbyI-wezk3n_gADj6U0kgAIJQ1neZv1KtxJk5fsmH24x6VrXg5ZC6GYKW-B0XLN5n33S7ZHDq7D-2-DUKvV3BtlODc5a1kQODlDYCd976-_GZo577LvcRrzsgPNAlo6'
+              'assets/images/bouquets/bouquet-of-orange-lilies.jpg'
           }
         },
         {
           src:
-            'https://flostyle.ru/images/stories/virtuemart/product/eMLfVALh7S0.jpg',
+            'assets/images/bouquets/lush-bouquet-of-23-peonies.jpg',
           opts: {
             caption: 'Пышный букет из 23 пионов',
             thumb:
-              'https://flostyle.ru/images/stories/virtuemart/product/eMLfVALh7S0.jpg'
+              'assets/images/bouquets/lush-bouquet-of-23-peonies.jpg'
           }
         },
         {
           src:
-            'https://kiwiflowershop.com.ua/image/cache/catalog/photo_2020-10-11_18-00-40-601x800.jpg',
+            'assets/images/bouquets/mono-bouquet-of-painted-tulips.jpg',
           opts: {
             caption: 'Моно букет из крашенных тюльпанов',
             thumb:
-              'https://kiwiflowershop.com.ua/image/cache/catalog/photo_2020-10-11_18-00-40-601x800.jpg'
+              'assets/images/bouquets/mono-bouquet-of-painted-tulips.jpg'
           }
         },
         {
           src:
-            'https://www.roza4u.ru/image/cache/catalog/buket-tulips-columbus/kolumbus2-1400x1400.jpg',
+            'assets/images/bouquets/bouquet-of-columbus-tulips.jpg',
           opts: {
             caption: 'Букет тюльпанов Коламбус',
             thumb:
-              'https://www.roza4u.ru/image/cache/catalog/buket-tulips-columbus/kolumbus2-1400x1400.jpg'
+              'assets/images/bouquets/bouquet-of-columbus-tulips.jpg'
           }
         },
         {
           src:
-            'https://beru-buket.ru/images/stories/virtuemart/product/yaAUuw7LuYo.jpg',
+            'assets/images/bouquets/bouquet-of-hydrangea-3-colors.jpg',
           opts: {
             caption: 'Букет гортензий 3 цвета',
             thumb:
-              'https://beru-buket.ru/images/stories/virtuemart/product/yaAUuw7LuYo.jpg'
+              'assets/images/bouquets/bouquet-of-hydrangea-3-colors.jpg'
           }
         }
       ],
